6d2f2a52f358b4f47932dc2df53152e7971435cd
Luana Mesquita <luanacarvalhomesquita@gmail.com>
2023-11-01 21:25:23 +0000

    feat: share shortcuts

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -12,13 +12,34 @@
     </header>
 
     <main class="container shadow shadow-secondary_300 mx-auto p-4 w-full h-full min-h-screen my-4">
+        <!-- Feedback -->
         <div v-if="flashSuccess" class="success">
             {{ flashSuccess }}
         </div>
-        <div class="flex py-5 " v-if="user && isMain">
+
+        <!-- Buttons -->
+        <div class="w-full flex justify-end">
+            <div class="lg:w-1/6 md:w-1/2 w-full flex justify-end">
+                <div
+                    class="bg-white hover:shadow-md hover:border-primary_300 border cursor-pointer border-primary rounded-sm shadow-sm shadow-gray-300 p-1 dark:bg-gray-800 dark:hover:dark:bg-gray-700 justify-center flex items-center w-full rounded-r-sm"
+                    @click="share"
+                >
+                    <span class="mr-4 text-primary text-sm">COMPARTILHAR</span>
+                    <div class="flex pt-1">
+                        <div class="flex justify-center items-center">
+                            <img src="/icons/share.svg" alt="Search" class="icon-sm"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Welcome -->
+        <div class="flex bg-gray-100 p-4 my-4" v-if="user && isMain">
             <span>Olá, {{ user.name }}! Que tal pegar um atalho hoje? :)</span>
         </div>
 
+        <!-- Content -->
         <div>
             <slot>Default</slot>
         </div>
@@ -39,4 +60,6 @@ const flashSuccess = computed(() => page.props.flash.success)
 const user = computed(
     () => page.props.user
 )
+
+const share = () => window.alert('oi')
 </script>
